42bb39bfaad98ef1890ee9bfc6fe743c9cd0a449
Wangmei <wangmei@eixiangjian.cn>
2018-02-11 11:00:02 +0800

    M 登录成功到主页

--- pages/login/login.js
+++ pages/login/login.js
@@ -119,37 +119,37 @@ Page({
     console.log(data);
     if (!data.isEmployee) {
       if ((!/^(\(\d{3,4}\)|\d{3,4}-|\s)?\d{7,14}$/.test(data.phone)) || (!(/^1(3|4|5|7|8)\d{9}$/.test(data.phone)))) {
-        showInfoModal('提示', '请填写正确的手机号码', false, function (res) { })
+        showInfoModal('提示', '请填写正确的手机号码', false, (res) => {})
         return false
       }
       if (!data.code) {
-        showInfoModal('提示', '请填写验证码', false, function (res) { })
+        showInfoModal('提示', '请填写验证码', false, (res) => {})
         return false
       }
 
     } else {
-      if (data.account == null || data.account.length == 0) {
-        showInfoModal('提示', '请填写员工账号', false)
+      if (data.account === '' || data.account.length === 0) {
+        showInfoModal('提示', '请填写员工账号', false, (res) => {})
         return;
       }
       if (!data.password) {
-        showInfoModal('提示', '请填写密码', false, function (res) { })
-        return;
+        showInfoModal('提示', '请填写密码', false, (res) => {})
+        // return;
       }
-      Api.verifyWoker(data.account, data.password, {
-        success: function (e) {
-          console.log(e);
-
-        },
-      })
+      // Api.verifyWoker(data.account, data.password, {
+      //   success: function (e) {
+      //     console.log(e);
+      //
+      //   },
+      // })
     }
 
     // 请求登录(登录时，微信授权获取用户昵称和头像，作为用户资料)
     //登录成功，判断用户角色(用户或工作人员)
     //记录登录状态：已登录状态，直接打开小程序首页
-    // wx.switchTab({
-    //   url: `/pages/index/index` //登录成功后到主页
-    // })
+    wx.switchTab({
+      url: `/pages/index/index` //登录成功后到主页
+    })
   },
 
   // 员工登录
