8e8e3eb87b0475d91fea30bef461b93e9aeaee0b
Wangmei <wangmei@eixiangjian.cn>
2018-02-11 10:54:33 +0800

    M 登录页UI调整

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,78 +1,108 @@
 //login.js
-  import {showInfoModal} from '../../utils/wxJs'
+import { showInfoModal } from '../../utils/wxJs'
 
+// var Api = require('../../api/api.js');
+// var Data = require('../../api/data.js');
 //获取应用实例
 const app = getApp()
 
 Page({
   data: {
-    phone: null,
-    code: null,
-    flag : false,
-    sendCodeText : "发送验证码",
+    phone: '', // 用户手机号
+    account: '', // 员工账号
+    code: '', // 验证码
+    flag: false,
+    sendCodeText: "发送验证码",
     isEmployee: false, //是否是员工,
-    password: null,
+    password: '', // 员工密码
     codeTime: 60, //验证码倒计时
-    codeDis: false //按钮点击状态，是否禁用
+    codeDis: false, //按钮点击状态，是否禁用
+    loginBtnActive: false // 是否激活按钮
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
   onLoad: function () {
   },
-  inputEnter (e) {
+  inputEnter(e) {
+    let data = this.data
     var type = e.currentTarget.dataset.type;
     var value = e.detail.value;
-    switch (type){
+    switch (type) {
       case 'phone':
         this.setData({
-          phone:value
+          phone: value
+        })
+        break;
+      case 'account':
+        this.setData({
+          account: value
         })
         break;
       case 'code':
         this.setData({
-          code:value
+          code: value
         })
         break;
       case 'password':
         this.setData({
-          password:value
+          password: value
         })
         break;
+    }
+    if (type === 'password' && data.account !== '' && data.password !== '') { // 输入密码时
+      this.setData({
+        loginBtnActive: true // 激活登录按钮
+      })
+    }
+    if (type === 'password' && data.account !== '' && data.password === '') {
+      this.setData({
+        loginBtnActive: false // 取消激活登录按钮
+      })
+    }
+    if (type === 'code' && data.phone !== '' && data.code !== '') { // 输入验证码时
+      this.setData({
+        loginBtnActive: true // 激活登录按钮
+      })
+    }
+    if (type === 'code' && data.phone !== '' && data.code === '') {
+      this.setData({
+        loginBtnActive: false // 取消激活登录按钮
+      })
     }
   },
 
   // 发送验证码倒计时
-  codeTime (data) {
+  codeTime(data) {
     var that = this;
     that.setData({
-      sendCodeText : 60
+      sendCodeText: 60
     })
-    let time = setInterval(()=>{
+    let time = setInterval(() => {
       let sendCodeText = data.sendCodeText
-      sendCodeText --;
+      sendCodeText--;
       that.setData({
-        sendCodeText : sendCodeText
+        sendCodeText: sendCodeText
       })
-      if(sendCodeText === 0){
+      if (sendCodeText === 0) {
         clearInterval(time)
         that.setData({
-          sendCodeText : "获取验证码",
-          codeDis : false
+          sendCodeText: "获取验证码",
+          codeDis: false
         })
       }
-    },1000)
+    }, 1000)
   },
 
   // 发送验证码
-  sendCode (e) {
+  sendCode(e) {
     var that = this
     var data = that.data
     if ((!/^(\(\d{3,4}\)|\d{3,4}-|\s)?\d{7,14}$/.test(data.phone)) || (!(/^1(3|4|5|7|8)\d{9}$/.test(data.phone)))) {
-      showInfoModal('提示', '请填写正确的手机号码', false, function (res) {})
+      showInfoModal('提示', '请填写正确的手机号码', false, function (res) { })
     } else {
       that.setData({
         codeDis: true
@@ -84,41 +114,53 @@ Page({
   },
 
   // 登录
-  login () {
+  login() {
     var data = this.data
-    // if (!data.isEmployee) {
-    //   if ((!/^(\(\d{3,4}\)|\d{3,4}-|\s)?\d{7,14}$/.test(data.phone)) || (!(/^1(3|4|5|7|8)\d{9}$/.test(data.phone)))) {
-    //     showInfoModal('提示', '请填写正确的手机号码', false, function (res) {})
-    //   }
-    //   if (!data.code) {
-    //     showInfoModal('提示', '请填写验证码', false, function (res) {})
-    //   }
-    // } else {
-    //   if (!data.password) {
-    //     showInfoModal('提示', '请填写密码', false, function (res) {})
-    //   }
-    //   if ((!/^(\(\d{3,4}\)|\d{3,4}-|\s)?\d{7,14}$/.test(data.phone)) || (!(/^1(3|4|5|7|8)\d{9}$/.test(data.phone)))) {
-    //     showInfoModal('提示', '请填写正确的手机号码', false)
-    //   }
-    // }
+    console.log(data);
+    if (!data.isEmployee) {
+      if ((!/^(\(\d{3,4}\)|\d{3,4}-|\s)?\d{7,14}$/.test(data.phone)) || (!(/^1(3|4|5|7|8)\d{9}$/.test(data.phone)))) {
+        showInfoModal('提示', '请填写正确的手机号码', false, function (res) { })
+        return false
+      }
+      if (!data.code) {
+        showInfoModal('提示', '请填写验证码', false, function (res) { })
+        return false
+      }
+
+    } else {
+      if (data.account == null || data.account.length == 0) {
+        showInfoModal('提示', '请填写员工账号', false)
+        return;
+      }
+      if (!data.password) {
+        showInfoModal('提示', '请填写密码', false, function (res) { })
+        return;
+      }
+      Api.verifyWoker(data.account, data.password, {
+        success: function (e) {
+          console.log(e);
+
+        },
+      })
+    }
 
     // 请求登录(登录时，微信授权获取用户昵称和头像，作为用户资料)
     //登录成功，判断用户角色(用户或工作人员)
     //记录登录状态：已登录状态，直接打开小程序首页
-    wx.switchTab({
-      url: `/pages/index/index` //登录成功后到主页
-    })
+    // wx.switchTab({
+    //   url: `/pages/index/index` //登录成功后到主页
+    // })
   },
 
   // 员工登录
-  goEmployeeLogin () {
+  goEmployeeLogin() {
     var that = this
     that.setData({
       isEmployee: !that.data.isEmployee
     })
   },
 
-  goProtocol () {
+  goProtocol() {
     wx.navigateTo({
       url: '/pages/protocols/protocols'
     })
